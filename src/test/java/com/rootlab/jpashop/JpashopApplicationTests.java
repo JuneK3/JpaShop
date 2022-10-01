62a2e823c01cb743f238d708ae123940430c5e8f
Junek3 <rlarudrms28@gmail.com>
2022-10-01 15:44:16 +0900

    first commit

---
+++ src/test/java/com/rootlab/jpashop/JpashopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootlab.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
