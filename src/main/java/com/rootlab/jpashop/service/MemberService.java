0282aa2d061897f3bd8168bf5cce743e7fa79db6
Junek3 <rlarudrms28@gmail.com>
2022-10-11 09:45:59 +0900

    회원도메인 개발 및 회원기능 테스트

---
+++ src/main/java/com/rootlab/jpashop/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.rootlab.jpashop.service;
+
+import com.rootlab.jpashop.domain.Member;
+import com.rootlab.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberService {
+	private final MemberRepository memberRepository;
+
+	@Transactional
+	public Long join(Member member) {
+		validateDuplicateMember(member); //중복 회원 검증
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		List<Member> findMembers = memberRepository.findByName(member.getName());
+		if (!findMembers.isEmpty()) {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		}
+	}
+
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
+
+	public Member findOne(Long memberId) {
+		return memberRepository.findOne(memberId);
+	}
+
+}
