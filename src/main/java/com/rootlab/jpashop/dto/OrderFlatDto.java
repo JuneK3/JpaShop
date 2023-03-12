2fd2dec6f99ef7e0535a797eef3e7c50e1ac6b27
Junek3 <rlarudrms28@gmail.com>
2023-03-13 02:19:14 +0900

    feat: API 개발 고급 - 컬렉션 조회 최적화

---
+++ src/main/java/com/rootlab/jpashop/dto/OrderFlatDto.java
@@ -0,0 +1,31 @@
+package com.rootlab.jpashop.dto;
+
+import com.rootlab.jpashop.domain.Address;
+import com.rootlab.jpashop.domain.status.OrderStatus;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class OrderFlatDto {
+    private Long orderId;
+    private String name;
+    private LocalDateTime orderDate;
+    private Address address;
+    private OrderStatus orderStatus;
+    private String itemName;
+    private int orderPrice;
+    private int count;
+
+    public OrderFlatDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus,
+                        Address address, String itemName, int orderPrice, int count) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.itemName = itemName;
+        this.orderPrice = orderPrice;
+        this.count = count;
+    }
+}
